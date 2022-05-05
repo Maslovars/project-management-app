e2b1b993d6535538b97eea661595f7b6b90d9b6f
Voloshin_Sergey <4voloshin@gmail.com>
2022-05-06 01:08:03 +0300

    feat: add stylelint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      HI
-    </div>
-  )
-}
+  return <div>HI</div>;
+};
 
-export default Home
+export default Home;
