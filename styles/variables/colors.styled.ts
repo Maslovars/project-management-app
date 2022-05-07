bf0aafe92f8f18ea9acf937658e809661b3664cc
Voloshin_Sergey <4voloshin@gmail.com>
2022-05-07 10:13:56 +0300

    fix: conflicts

---
+++ styles/variables/colors.styled.ts
@@ -0,0 +1,17 @@
+export const colors = {
+colorBgPrimary: '#eeebe5',
+colorBgPrimaryLighten: '#f7f6f3',
+colorBgSecondary: '#ffffff',
+colorBgSecondaryDarken: '#eaeaea',
+
+colorFontPrimary: '#131313',
+colorFontPrimaryLighten: '#1d201c',
+colorFontSecondary: '#9b9b9b',
+
+colorSuccess: '#7eb973',
+colorSuccessLight: '#cef9c6',
+colorError: '#ff0000',
+colorErrorLight: '#ffe2e2',
+colorInfo: '#ffc200',
+colorInfoLight: '#fff8dd',
+};
