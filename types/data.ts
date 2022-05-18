aa3d0b257d6d167193e4c4911ebd62c1ae1e42c2
Voloshin Sergei <4voloshin@gmail.com>
2022-05-18 10:33:35 +0300

    refactor: updated types for Board/Column/Task

--- types/data.ts
+++ types/data.ts
@@ -1,18 +1,32 @@
-export interface TaskTypes {
-  id: number;
+export interface BoardType {
+  id: string;
   title: string;
   description: string;
-  avatar: string;
-  name: string;
-  fileImage: string;
-}
-
-export interface TaskListTypes {
-  tasks: TaskTypes[];
 }
 
 export interface ColumnTypes {
-  id: number;
+  id: string;
   title: string;
+  order: number;
+}
+
+export interface FileType {
+  filename: string;
+  fileSize: number;
+}
+
+export interface TaskTypes {
+  id: string;
+  title: string;
+  done: boolean;
+  order: number;
+  description: string;
+  userId: string;
+  boardId: string;
+  columnId: string;
+  files: FileType[];
+}
+
+export interface TaskListTypes {
   tasks: TaskTypes[];
 }
