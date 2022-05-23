58cc561a70b17957b30f4aa948b765acee617e7b
Voloshin-Sergei <4voloshin@gmail.com>
2022-05-24 01:07:50 +0300

    feat: #52 added styles for FileInput

--- components/common/FileInput/FileInput.styled.ts
+++ components/common/FileInput/FileInput.styled.ts
@@ -1,36 +1,57 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { colors } from '@/variables/colors.styled';
 import { fonts } from '@/variables/fonts.styled';
 
-export const Container = styled.div`
-  padding: 1rem;
-`;
-export const Title = styled.span`
-  color: ${colors.colorFontSecondary};
-  font-weight: ${fonts.fontWeightRegular};
-  font-size: 1.4rem;
-  transition: all 0.3s ease;
-`;
-export const Label = styled.label`
-  display: block;
-  width: 10rem;
-  text-align: center;
+interface LabelProps {
+  empty: boolean;
+}
+
+const Empty = css`
   border: 0.2rem dashed ${colors.colorFontSecondary};
-  border-radius: 0.4rem;
-  cursor: pointer;
-  transition: all 0.3s ease;
 
   :hover {
     border: 0.2rem solid ${colors.colorSuccess};
   }
+`;
+
+const Full = css`
+  border: 0.2rem solid ${colors.colorSuccess};
+`;
+
+export const Container = styled.div`
+  padding: 1rem;
+`;
+
+export const Title = styled.span<LabelProps>`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-weight: ${fonts.fontWeightRegular};
+  font-size: 1.4rem;
+  transition: all 0.3s ease;
+  ${(props) =>
+    props.empty ? `color: ${colors.colorFontSecondary}` : `color: ${colors.colorSuccess}`};
+`;
+
+export const Label = styled.label<LabelProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.4rem;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  padding: 1rem;
 
   &:hover ${Title} {
     color: ${colors.colorSuccess};
   }
+
+  ${(props) => (props.empty ? Empty : Full)};
 `;
 
 export const Input = styled.input`
   width: 1px;
+  height: 1px;
   outline: 0;
   opacity: 0;
   user-select: none;
